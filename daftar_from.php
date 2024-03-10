b687fd3e05d1c164c167998bffe3bb6c43ada80f
Tiahh1184 <146157083+Tiahh1184@users.noreply.github.com>
2024-03-10 23:30:04 +0700

    tugas week2

---
+++ daftar_from.php
@@ -0,0 +1,187 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form>
+  <div class="form-group row">
+    <label for="nim" class="col-4 col-form-label">NIM</label> 
+    <div class="col-8">
+      <input id="nim" name="nim" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="NL" class="col-4 col-form-label">Nama Lengkap</label> 
+    <div class="col-8">
+      <input id="NL" name="NL" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Jenis Kelamin</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="jk" id="jk_0" type="radio" class="custom-control-input" value="Laki - Laki"> 
+        <label for="jk_0" class="custom-control-label">Laki - Laki</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="jk" id="jk_1" type="radio" class="custom-control-input" value="Perempuan"> 
+        <label for="jk_1" class="custom-control-label">Perempuan</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="ps" class="col-4 col-form-label">Program Studi</label> 
+    <div class="col-8">
+      <select id="ps" name="ps" class="custom-select">
+        <option value="Sistem Informasi">Sistem Informasi</option>
+        <option value="Tehnik Informatika">Tehnik Informatika</option>
+        <option value="Bisnis Digital">Bisnis Digital</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Skill Web &amp; Programing</label> 
+    <div class="col-8">
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_0" type="checkbox" checked="checked" class="custom-control-input" value="rabbit"> 
+        <label for="sw_0" class="custom-control-label">HTML</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_1" type="checkbox" class="custom-control-input" value="CSS"> 
+        <label for="sw_1" class="custom-control-label">CSS</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_2" type="checkbox" class="custom-control-input" value="Java Scipt"> 
+        <label for="sw_2" class="custom-control-label">Java Scipt</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_3" type="checkbox" class="custom-control-input" value="RWD Bootstrap"> 
+        <label for="sw_3" class="custom-control-label">RWD Bootstrap</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_4" type="checkbox" class="custom-control-input" value="PHP"> 
+        <label for="sw_4" class="custom-control-label">PHP</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_5" type="checkbox" class="custom-control-input" value="Python"> 
+        <label for="sw_5" class="custom-control-label">Python</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_6" type="checkbox" class="custom-control-input" value="Java"> 
+        <label for="sw_6" class="custom-control-label">Java</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="td" class="col-4 col-form-label">Tempat Domisili</label> 
+    <div class="col-8">
+      <select id="td" name="td" class="custom-select">
+        <option value="Jakarta">Jakarta</option>
+        <option value="Depok">Depok</option>
+        <option value="Bogor">Bogor</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="email" class="col-4 col-form-label">Email</label> 
+    <div class="col-8">
+      <input id="email" name="email" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form>
+  <div class="form-group row">
+    <label for="nim" class="col-4 col-form-label">NIM</label> 
+    <div class="col-8">
+      <input id="nim" name="nim" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="NL" class="col-4 col-form-label">Nama Lengkap</label> 
+    <div class="col-8">
+      <input id="NL" name="NL" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Jenis Kelamin</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="jk" id="jk_0" type="radio" class="custom-control-input" value="Laki - Laki"> 
+        <label for="jk_0" class="custom-control-label">Laki - Laki</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="jk" id="jk_1" type="radio" class="custom-control-input" value="Perempuan"> 
+        <label for="jk_1" class="custom-control-label">Perempuan</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="ps" class="col-4 col-form-label">Program Studi</label> 
+    <div class="col-8">
+      <select id="ps" name="ps" class="custom-select">
+        <option value="Sistem Informasi">Sistem Informasi</option>
+        <option value="Tehnik Informatika">Tehnik Informatika</option>
+        <option value="Bisnis Digital">Bisnis Digital</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Skill Web &amp; Programing</label> 
+    <div class="col-8">
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_0" type="checkbox" checked="checked" class="custom-control-input" value="rabbit"> 
+        <label for="sw_0" class="custom-control-label">HTML</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_1" type="checkbox" class="custom-control-input" value="CSS"> 
+        <label for="sw_1" class="custom-control-label">CSS</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_2" type="checkbox" class="custom-control-input" value="Java Scipt"> 
+        <label for="sw_2" class="custom-control-label">Java Scipt</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_3" type="checkbox" class="custom-control-input" value="RWD Bootstrap"> 
+        <label for="sw_3" class="custom-control-label">RWD Bootstrap</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_4" type="checkbox" class="custom-control-input" value="PHP"> 
+        <label for="sw_4" class="custom-control-label">PHP</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_5" type="checkbox" class="custom-control-input" value="Python"> 
+        <label for="sw_5" class="custom-control-label">Python</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="sw" id="sw_6" type="checkbox" class="custom-control-input" value="Java"> 
+        <label for="sw_6" class="custom-control-label">Java</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="td" class="col-4 col-form-label">Tempat Domisili</label> 
+    <div class="col-8">
+      <select id="td" name="td" class="custom-select">
+        <option value="Jakarta">Jakarta</option>
+        <option value="Depok">Depok</option>
+        <option value="Bogor">Bogor</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="email" class="col-4 col-form-label">Email</label> 
+    <div class="col-8">
+      <input id="email" name="email" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
